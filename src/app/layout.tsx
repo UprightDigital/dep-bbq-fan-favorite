4ff4a0b9e063447ec9b8bbba05b261e53355c6b6
Payton Holcomb <payton.holcomb@uprightdigital.com>
2026-07-30 15:54:33 -0500

    Use real Barbaro Roman and Adobe Handwriting Frank fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,5 @@
 import type { Metadata } from "next";
-import { Rye, Oswald, Caveat } from "next/font/google";
 import "./globals.css";
-
-const rye = Rye({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-display",
-});
-
-const oswald = Oswald({
-  subsets: ["latin"],
-  variable: "--font-condensed",
-});
-
-const caveat = Caveat({
-  subsets: ["latin"],
-  variable: "--font-script",
-});
 
 export const metadata: Metadata = {
   title: "Fan Favorite Voting | Permian Basin BBQ Cook-Off",
@@ -30,10 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${rye.variable} ${oswald.variable} ${caveat.variable} h-full`}
-    >
+    <html lang="en" className="h-full">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/qgc1stk.css" />
+      </head>
       <body className="min-h-full flex flex-col bg-bbq-black text-bbq-white font-body antialiased">
         {children}
       </body>
